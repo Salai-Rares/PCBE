c654400d5671e4df6f8dc2207e0d3485973b48a7
Salai-Rares <salaicostel@yahoo.com>
2021-11-11 21:00:34 +0200

    Simple producer consumer architecture

---
+++ src/main/java/com/exemple/kafka/Consumer.java
@@ -0,0 +1,67 @@
+package com.exemple.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+
+public class Consumer {
+    private String name;
+    final private KafkaConsumer<String,String> consumer;
+    private List<String> topics;
+    public Consumer(String name,List<String> topics){
+        final String bootstrapServer = "localhost:9092";
+        Properties p = new Properties();
+        p.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServer);
+        p.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        p.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class.getName());
+        p.setProperty(ConsumerConfig.GROUP_ID_CONFIG,"java-group-consumer");
+        p.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"earliest");
+        this.name=name;
+        this.topics = new ArrayList<>(topics);
+        consumer=new KafkaConsumer<String, String>(p);
+        consumer.subscribe(topics);
+    }
+    public void addSubscription(List<String> topicName){
+        topics.addAll(topicName);
+        consumer.subscribe(topics);
+    }
+    public void viewSubscribedTopics(){
+        int contor = 0;
+        for(String topic:topics){
+            System.out.println(topic+" "+contor);
+            contor++;
+        }
+    }
+
+    public KafkaConsumer getKafkaConsumer(){
+        return consumer;
+    }
+
+    public static void main(String[] args) {
+        //Create logger for class
+        final Logger logger = LoggerFactory.getLogger(Consumer.class);
+        Consumer consumer1 = new Consumer("FirstConsumer",Arrays.asList("TestTopic"));
+        KafkaConsumer<String,String> consumer = consumer1.getKafkaConsumer();
+        //create consumer
+        consumer1.addSubscription(Arrays.asList("NewTopic","Animale"));
+
+        while(true){
+            ConsumerRecords<String,String> records = consumer.poll(Duration.ofMillis(1000));
+            for(ConsumerRecord consumerRecord:records){
+                System.out.println("Topic "+ consumerRecord.topic()+" Key "+consumerRecord.key()+" value "+consumerRecord.value());
+            }
+        }
+
+    }
+}
---
+++ src/main/java/com/exemple/kafka/Producer.java
@@ -0,0 +1,47 @@
+package com.exemple.kafka;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class Producer extends Thread{
+
+    public static void main(String[] args) {
+        final Logger logger = LoggerFactory.getLogger(Producer.class);
+    //Create properties object for Producer
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+
+        //key.serializer
+        //value.serializer
+        //bootstrap.servers
+    //Create the Producer
+    final KafkaProducer<String,String> producer = new KafkaProducer<String, String>(properties);
+    //Create the ProducerRecord
+        ProducerRecord<String,String> record = new ProducerRecord<>("NewTopic","Titlu3","Mesaj2");
+    //Send Data - Asynchronous
+    producer.send(record, new Callback() {
+        @Override
+        public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+            if(e==null){
+                logger.info("\nReceived record metadata\n" +
+                        "Topic"+ recordMetadata.topic() + ", Partition" + recordMetadata.partition()+", "+
+                "Offset"+recordMetadata.offset() + "@ Timestamp" + recordMetadata.timestamp()+"\n");
+//                System.out.println("Topic"+ recordMetadata.topic() + ", Partition" + recordMetadata.partition()+", "+
+//                        "Offset"+recordMetadata.offset() + "@ Timestamp" + recordMetadata.timestamp()+"\n");
+            }
+            else{
+                logger.error("Error occured",e);
+            }
+        }
+    });
+    //flush and close producer
+        producer.flush();
+        producer.close();
+    }
+}
